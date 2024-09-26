268e911ce8694b8669e8e97a1de106f833e60ef9
KhairallahA <khairallahfreelancer@gmail.com>
2024-09-27 00:19:48 +0300

    Deploy examples

---
+++ README.md
@@ -0,0 +1,23 @@
+# Rust DApps
+
+Implementation of Rust Decentralized Applications.
+
+## Message DApp
+
+You can run the project by adding these commands:
+
+```
+cd message_dapp
+cp .env.example .env
+cargo run
+```
+
+## Conduits
+
+You can run the project by adding these commands:
+
+```
+cd conduits
+cp .env.example .env
+cargo run
+```
---
+++ message_dapp/src/main.rs
@@ -0,0 +1,30 @@
+pub mod sync_messages;
+use pwr_rs::Wallet;
+use tokio::io::{self, AsyncBufReadExt, BufReader};
+use tokio::spawn;
+use dotenvy::dotenv;
+use sync_messages::sync;
+use std::env;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    // Setting up your wallet in the SDK
+    let private_key = env::var("PRIVATE_KEY").unwrap();
+    let wallet = Wallet::from_hex(&private_key).unwrap();
+    let vm_id: u64 = 1234;
+    let stdin = io::stdin();
+    let reader = BufReader::new(stdin);
+    let mut lines = reader.lines();
+    
+    spawn(sync());
+    
+    while let Some(message) = lines.next_line().await? {
+        let obj = serde_json::json!({ "message": message });
+        let data = serde_json::to_vec(&obj)?;
+
+        // Send the VM data
+        wallet.send_vm_data(vm_id, data).await;
+    }
+    Ok(())
+}
